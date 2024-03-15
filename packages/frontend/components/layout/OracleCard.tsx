ecc2e147fcae56a4ac779e11b41cad0f0c4b1135
Epitome <defibizzylos@gmail.com>
2024-03-15 13:08:53 +0800

    remove theme import for vercel

--- packages/frontend/components/layout/OracleCard.tsx
+++ packages/frontend/components/layout/OracleCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box, useColorModeValue } from '@chakra-ui/react'
-import theme from '../../styles/theme'
 interface OracleCardProps {
   children: React.ReactNode
 }
@@ -9,7 +8,6 @@ export const OracleCard = (props: OracleCardProps): JSX.Element => {
   const bg = useColorModeValue('gray.100', 'yellow.900')
   return (
     <>
-      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
       <Box 
             boxShadow='dark-lg' borderWidth='1px' borderRadius='lg' maxWidth="container.sm" p="8" mt="8" bg={bg} {...props}></Box>
     </>
